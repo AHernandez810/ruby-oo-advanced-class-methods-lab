5b58f52e5e64f4d713feafc3be9adab1c2bb95d4
ashley <ashleyhernandez810.ah@gmail.com>
2020-11-09 01:45:56 -0500

    commit tree 59aec886df934ee7642e8b08158027db7fd4b64a

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new 
+    self.all << song 
+    song
+  end 
+
+  def self.new_by_name(song_title)
+    song = self.new
+    song.name = song_title
+    song
+  end 
+
+  def self.create_by_name(song_title)
+    song = self.create
+    song.name = song_title
+    song
+  end 
+
+  def self.find_by_name(song_title)
+    song = self.all.find{|song| song.name== song_title }
+    song
+  end
+
+  def self.find_or_create_by_name(song_title)
+    song = self.find_by_name(song_title)
+    if song
+      song 
+    else 
+      self.create_by_name(song_title)
+    end 
+  end 
+
+  def self.alphabetical
+    sorted_songs = self.all.sort_by{|song| song.name}
+    sorted_songs
+  end 
+
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ")[0])
+    song
+  end 
+
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ")[0])
+    @@all << song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+  
 end
